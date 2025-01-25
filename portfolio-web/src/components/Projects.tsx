452006cdf8c81edf08ffe0965681586a0b0cf85d
Jacek Kozłowski <jacek.kozlowski2000@gmail.com>
2025-01-25 23:27:03 +0100

    Version 0.0.4.1

--- portfolio-web/src/components/Projects.tsx
+++ portfolio-web/src/components/Projects.tsx
@@ -3,14 +3,14 @@ import workInProgress from '../assets/img/test.jpg'
 
 const Projects = () => {
   return (
-    <section className='text-white bg-bg_color_1 flex flex-col justify-center items-center min-h-screen w-full pt-[100px] px-[9%] pv-[20px]' id='projects'>
-      <h2>My <span className='text-main_accent'>Project</span></h2>
+    <section className='text-white bg-bg_color_2 flex flex-col justify-center items-center min-h-screen w-full pt-[100px] px-[9%] pv-[20px]' id='projects'>
+      <h2 className='text-center text-5xl font-bold'>My <span className='text-main_accent'>Project</span></h2>
     
-      <div className=''>
+      <div className='grid grid-cols-1 md:grid-cols-3 items-center gap-5'>
 
-        <div className=''>
-          <img src={workInProgress} alt="" />
-          <div>
+        <div className='relative border'>
+          <img className='w-[100%] transition-all duration-500 ease hover:scale-105' src={workInProgress} alt="" />
+          <div className='absolute bottom-0 left-0 w-[100%] h-[100%] bg-gradient-to-b from-[rgba(0,0,0,0.1)] to-main_accent flex justify-center items-center flex-col text-center px-16 translate-y-[100%] transition-all duration-500 ease translate-y-[0%]'>
             <h4>Web Design</h4>
             <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit.
