3bbb2365cf9d14172f6fd3e4bd594307fac478a6
Jacek Kozłowski <jacek.kozlowski2000@gmail.com>
2025-02-25 23:56:07 +0100

    Version 0.1.2.3

--- portfolio-web/src/pages/About.tsx
+++ portfolio-web/src/pages/About.tsx
@@ -1,19 +1,25 @@
-import React from 'react'
+import React, from 'react'
 import aboutPhoto from '../assets/img/Ja i papuga.png'
+import { useTranslation } from 'react-i18next'
+
+
 const About = () => {
+
+  const { t } = useTranslation()
+
   return (
     <section className='bg-bg_color_2 flex justify-center items-center min-h-screen w-full pt-[100px] px-[9%] pv-[20px]' id='about'>
       <div className='w-[75vw] m-20'>
        <img src={aboutPhoto} alt="" className='max-w-[100%] max-h-[100%] animate-floatImage'/>
       </div>
       <div className='text-white w-full'>
-        <h2 className='text-left text-5xl font-semibold mb-2'>About <span className='text-main_accent'>Me</span></h2>
+        <h2 className='text-left text-5xl font-semibold mb-2'>{t("about.about")}<span className='text-main_accent'> {t("about.me")}</span></h2>
         <h3 className='text-3xl font-bold mb-4'>Front developer</h3>
         <p className='text-justify leading-6 my-5 mb-5'>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur
           dolorum corporis aliquam facilis nihil mollitia et consectetur quod,
           vero necessitatibus labore nesciunt. Aspernatur officiis mollitia,
-          debitis quae at eaque distinctio.
+          debitis quae at eaque distinctio. <a href='#' className='cursor-pointer text-main_accent hover:drop-shadow-custom-glow transition-all'>See more..</a>
         </p>
         <div className='flex justify-center gap-10'>
           <a href="#">
--- portfolio-web/src/pages/Home.tsx
+++ portfolio-web/src/pages/Home.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import 'boxicons/css/boxicons.min.css';
 import homePhoto from '../assets/img/Image 1.png'
 import useSmoothScroll from '../hooks/useSmoothScroll';
-import { Trans, useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 
 const Home = () => {
 
@@ -51,10 +51,7 @@ const Home = () => {
         <h3 className='text-3xl font-semibold mb-3'>{t("home.hello")}</h3>
         <h1 className='text-6xl font-semibold'>Jacek Kozłowski</h1>
         <h3 className='mt-4 text-3xl font-semibold mb-5'>{t("home.im")} <span className='text-main_accent'>{text}</span><span className='animate-blink'>|</span></h3>
-        <p className='leading-6 '>
-          {/* {t("home.text")} */}
-          <Trans i18nKey="home.text" components={{ br: <br /> }}/>
-        </p>
+        <p className='leading-7'>{t("home.text")}</p>
         <div className='flex space-x-4 mt-5'>
           <a href="#"><i className='bx bxl-instagram-alt text-main_accent inline-flex justify-center items-center text-3xl bg-opacity-0 border-main_accent rounded-full p-2 border-[3px] hover:animate-tada hover:bg-main_accent hover:text-bg_color_2 transition-all hover:drop-shadow-custom-glow'></i></a>
           <a href="#"><i className="bx bxl-youtube text-main_accent inline-flex justify-center items-center text-3xl bg-opacity-0 border-main_accent rounded-full p-2 border-[3px] hover:animate-tada hover:bg-main_accent hover:text-bg_color_2 transition-all hover:drop-shadow-custom-glow"></i></a>
@@ -62,7 +59,7 @@ const Home = () => {
           <a href="#"><i className="bx bxl-linkedin text-main_accent inline-flex justify-center items-center text-3xl bg-opacity-0 border-main_accent rounded-full p-2 border-[3px] hover:animate-tada hover:bg-main_accent hover:text-bg_color_2 transition-all hover:drop-shadow-custom-glow"></i></a>
           <a href="#"><i className="bx bx-coffee-togo text-main_accent inline-flex justify-center items-center text-3xl bg-opacity-0 border-main_accent rounded-full p-2 border-[3px] hover:animate-tada hover:bg-main_accent hover:text-bg_color_2 transition-all hover:drop-shadow-custom-glow"></i></a>
         </div>
-        <a onClick={() => scrollTo("contact")} className='bg-main_accent inline-block my-7 py-2 px-12 rounded-full text-bg_color_2 font-extrabold hover:drop-shadow-custom-glow hover:scale-110 transition-all duration-500 ease-in-out cursor-pointer text-xl shadow-custom-around shadow-main_accent'>Get in touch</a>
+        <a onClick={() => scrollTo("contact")} className='bg-main_accent inline-block my-7 py-2 px-12 rounded-full text-bg_color_2 font-extrabold hover:drop-shadow-custom-glow hover:scale-110 transition-all duration-500 ease-in-out cursor-pointer text-xl shadow-custom-around shadow-main_accent'>{t("home.btn_get_in_touch")}</a>
       </div>
       <div className='w-[75vw] m-20'>
         <img className='max-w-[100%] max-h-[100%] animate-floatImage' src={homePhoto} alt="" />
